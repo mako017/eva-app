b181f5e01c9a959ac990e9af0ea53ba874d7bef3
Marco Koch <marco.koch@uni-saarland.de>
2020-09-23 15:35:19 +0200

    deploy

---
+++ precache-manifest.a9ea8df9a48dd816d11cf387df306dcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d6d5ef8f045970d4756",
+    "url": "/eva-app/css/app.0c948009.css"
+  },
+  {
+    "revision": "ce9fe0c1dfc45084cf405af65370bbcf",
+    "url": "/eva-app/index.html"
+  },
+  {
+    "revision": "7d6d5ef8f045970d4756",
+    "url": "/eva-app/js/app.db4bdec5.js"
+  },
+  {
+    "revision": "be674190cb1678c6e257",
+    "url": "/eva-app/js/chunk-vendors.d124244e.js"
+  },
+  {
+    "revision": "e56c5fe2b74fa244bdfc3c62c2e94324",
+    "url": "/eva-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/eva-app/robots.txt"
+  }
+]);
