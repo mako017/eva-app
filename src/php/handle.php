35e7b8389d759af159b040c7f33e100edc1c1423
marco Koch <marco.koch@uni-saarland.de>
2020-11-13 17:43:24 +0100

    added scripts for writing data

--- src/php/handle.php
+++ src/php/handle.php
@@ -3,3 +3,19 @@
 require_once("config.php");
 require_once("saveData.php");
 require_once("verifyUser.php");
+
+$postData = json_decode(file_get_contents('php://input'));
+$call = $postData->call;
+$payload = $postData->payload;
+
+switch ($call) {
+    case 'transferData':
+        writeData($mysqli, $payload);
+        break;
+    
+    default:
+        # code...
+        break;
+}
+
+$sql->close();
--- src/php/saveData.php
+++ src/php/saveData.php
@@ -1,1 +1,13 @@
 <?php
+
+public function writeData($mysqli, $data)
+{
+    $sql = $mysqli->prepare("INSERT INTO `Evabox_Daten` (`lsf`, `datum`, `zeit`, `wertung`) VALUES (?, ?, ?, ?)");
+    $sql->bind_param("isss", $data->lsf, $data->datum, $data->zeit, $data->wertung);
+    $sql->execute();
+    if ($sql->affected_rows > 0) {
+            echo "1";
+    } else {
+            exit('0');
+    }
+}
