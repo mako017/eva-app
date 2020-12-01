6a81a3042268c87610c4dcc11563516077e17ab6
marco Koch <marco.koch@uni-saarland.de>
2020-12-01 18:55:14 +0100

    UI for saving courses

--- src/components/AdminArea/CourseAdmin.vue
+++ src/components/AdminArea/CourseAdmin.vue
@@ -1,12 +1,15 @@
 <template>
 	<div>
-		<CourseTable v-for="(course, id) in mockCourses" :key="id" :course="course" />
+		<CourseTable v-for="(course, id) in courses" :key="id" :course="course" />
 		<div class="newCourse">
 			<label for="">LSF: <input type="text" v-model="newCourse.lsf"/></label>
 			<label for="">Titel: <input type="text" v-model="newCourse.titel"/></label>
 			<i class="material-icons" @click="addCourse">
 				add_circle_outline
 			</i>
+		</div>
+		<div class="controlPanel">
+			<button @click="saveCourses(courses)">Save Courses</button>
 		</div>
 	</div>
 </template>
@@ -15,6 +18,7 @@
 import { Component, Vue } from "vue-property-decorator";
 import CourseTable from "@/components/AdminArea/CourseTable.vue";
 import { courseContainer } from "@/components/models.ts";
+import { saveCourses } from "@/assets/ts/courses.ts";
 
 @Component({
 	components: { CourseTable },
@@ -24,7 +28,7 @@ export default class CourseAdmin extends Vue {
 		lsf: "",
 		titel: "",
 	};
-	mockCourses: Array<courseContainer> = [
+	courses: Array<courseContainer> = [
 		{
 			id: 1,
 			lsf: 123456,
@@ -48,33 +52,10 @@ export default class CourseAdmin extends Vue {
 				},
 			],
 		},
-		{
-			id: 2,
-			lsf: 987654,
-			titel: "Pharmazie",
-			singleCourses: [
-				{
-					datum: "2020-11-30",
-					raum: "45 BIO",
-					von: "10:15",
-					bis: "11:45",
-					sitzung: "Antibiotika I",
-					dozent: "Prof. Dr. Paracelsus",
-				},
-				{
-					datum: "2020-12-07",
-					raum: "45 BIO",
-					von: "10:15",
-					bis: "11:45",
-					sitzung: "Antibiotika II",
-					dozent: "Prof. Dr. Paracelsus",
-				},
-			],
-		},
 	];
 	addCourse() {
-		this.mockCourses.push({
-			id: this.mockCourses.length > 0 ? this.mockCourses[this.mockCourses.length - 1].id + 1 : 1,
+		this.courses.push({
+			id: this.courses.length > 0 ? this.courses[this.courses.length - 1].id + 1 : 1,
 			lsf: +this.newCourse.lsf,
 			titel: this.newCourse.titel,
 			singleCourses: [],
@@ -95,5 +76,8 @@ export default class CourseAdmin extends Vue {
 		margin: 0 1% 0 auto;
 		cursor: pointer;
 	}
+}
+.controlPanel {
+	margin-top: 1rem;
 }
 </style>
