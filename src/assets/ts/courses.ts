dcd657047668273af5e29220eefd506b0a476dc5
Marco Koch <marco.koch@uni-saarland.de>
2020-12-03 15:54:08 +0100

    formatting

--- src/assets/ts/courses.ts
+++ src/assets/ts/courses.ts
@@ -2,41 +2,41 @@ import axios from "axios";
 import { courseContainer } from "@/components/models.ts";
 
 export function saveCourses(courses: Array<courseContainer>) {
-    axios
-        .post(
-            "./php/handle.php",
-            JSON.stringify({
-                call: "insertCourse",
-                payload: courses,
-            }),
-        )
-        .then(response => {
-            console.log(response.data);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+	axios
+		.post(
+			"./php/handle.php",
+			JSON.stringify({
+				call: "insertCourse",
+				payload: courses,
+			}),
+		)
+		.then(response => {
+			console.log(response.data);
+		})
+		.catch(error => {
+			console.log(error);
+		});
 }
 
 export function requestCourses() {
-    let unsortedCourses = [];
-    axios.post(
-        "./php/handle.php",
-        JSON.stringify({
-            call:"requestAllCourses"
-        })
-    )
-    .then(response => {
-        // console.log(response.data);
-        unsortedCourses = response.data;
-        console.log(unsortedCourses);
-        
-    })
-    .catch(error => {
-        console.log(error);
-    });
+	let unsortedCourses = [];
+	axios
+		.post(
+			"./php/handle.php",
+			JSON.stringify({
+				call: "requestAllCourses",
+			}),
+		)
+		.then(response => {
+			// console.log(response.data);
+			unsortedCourses = response.data;
+			console.log(unsortedCourses);
+		})
+		.catch(error => {
+			console.log(error);
+		});
 }
 
-function sortCourses(courses: Array<any>) {
-    let sortedArrays: Array<courseContainer> = []
-}
+// function sortCourses(courses: Array<any>) {
+//     let sortedArrays: Array<courseContainer> = []
+// }
