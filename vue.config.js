51fe2824ee37b337b92a267f3188e8b4102dfe51
Marco Koch <marco.koch@uni-saarland.de>
2020-12-04 16:26:02 +0100

    set up dev proxy

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
 	publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+	devServer: {
+		proxy: {
+			"^/php": {
+				target: "http://localhost/evabox",
+				changeOrigin: true,
+			},
+		},
+	},
 };
