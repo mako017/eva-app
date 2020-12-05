6f32811f0527ffc4c7dbe63634391f8eb1dd40ab
Marco Koch <marco.koch@uni-saarland.de>
2020-12-05 13:34:57 +0100

    activated navigation of admin area

--- src/components/AdminArea.vue
+++ src/components/AdminArea.vue
@@ -2,12 +2,12 @@
 	<div class="root">
 		<h1>Admin Area</h1>
 		<div class="nav">
-			<span>Course Administration</span>
-			<span>Data Export</span>
+			<span :class="{ active: navigation === 1 }" @click="navigation = 1">Course Administration</span>
+			<span :class="{ active: navigation === 2 }" @click="navigation = 2">Data Export</span>
 		</div>
 		<hr />
-		<DataExport />
-		<CourseAdmin />
+		<CourseAdmin v-if="navigation === 1" />
+		<DataExport v-else-if="navigation === 2" />
 	</div>
 </template>
 
@@ -22,11 +22,28 @@ import DataExport from "@/components/AdminArea/DataExport.vue";
 		CourseAdmin,
 	},
 })
-export default class AdminArea extends Vue {}
+export default class AdminArea extends Vue {
+	private navigation = 1;
+}
 </script>
 
 <style scoped lang="scss">
 .root {
 	width: 100%;
+	.nav {
+		width: 100%;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		span {
+			cursor: pointer;
+			&.active {
+				color: rgb(0, 138, 7);
+				font-weight: bold;
+			}
+			&:hover {
+				font-weight: bold;
+			}
+		}
+	}
 }
 </style>
