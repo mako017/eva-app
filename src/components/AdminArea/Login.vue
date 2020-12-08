1c8901de0dd6ac99238eeb7c596ed7e60dbba9c4
Marco Koch <marco.koch@uni-saarland.de>
2020-12-08 16:30:04 +0100

    layout for login form

--- src/components/AdminArea/Login.vue
+++ src/components/AdminArea/Login.vue
@@ -1,8 +1,13 @@
 <template>
 	<div class="root">
-		<label>User<input type="text" v-model="credentials.user"/></label>
-		<label>Password<input type="password" v-model="credentials.password"/></label>
-		<button type="button" @click.prevent.once="signIn">Sign In</button>
+		<h2>Login</h2>
+		<form class="login-form">
+			<label>User</label>
+			<input type="text" v-model="credentials.user" />
+			<label>Password</label>
+			<input type="password" v-model="credentials.password" />
+			<button type="button" @click.prevent.once="signIn">Sign In</button>
+		</form>
 	</div>
 </template>
 
@@ -31,6 +36,10 @@ export default class AdminLogin extends Vue {
 					token: response.data,
 					signedIn: true,
 				});
+				this.credentials = {
+					user: "",
+					password: "",
+				};
 			})
 			.catch(error => {
 				console.log(error);
@@ -39,4 +48,30 @@ export default class AdminLogin extends Vue {
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.root {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	* {
+		margin: 0.5rem 0;
+	}
+}
+.login-form {
+	display: grid;
+	grid-template-columns: 1fr 3fr;
+	gap: 0.5rem;
+	text-align: start;
+	border: 1px solid black;
+	padding: 0.4rem;
+	* {
+		margin: 0.2rem 0;
+	}
+	button {
+		justify-self: center;
+		grid-column-start: 1;
+		grid-column-end: 3;
+	}
+}
+</style>
