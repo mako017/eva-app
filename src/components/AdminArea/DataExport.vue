f17d6a1edc2127dd5c7189bef07042c9655f3e4a
marco Koch <marco.koch@uni-saarland.de>
2020-12-11 11:08:34 +0100

    layout for course export

--- src/components/AdminArea/DataExport.vue
+++ src/components/AdminArea/DataExport.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="root">
-		<CourseExport />
+		<CourseExport v-for="(course, id) in courses" :key="id" :course="course" />
 		<hr />
 		<RawExport />
 	</div>
@@ -10,6 +10,8 @@
 import { Component, Vue } from "vue-property-decorator";
 import CourseExport from "@/components/AdminArea/CourseExport.vue";
 import RawExport from "@/components/AdminArea/RawExport.vue";
+import { getAllCourses } from "@/assets/ts/courses.ts";
+import { courseContainer } from "@/components/models.ts";
 
 @Component({
 	components: {
@@ -17,7 +19,12 @@ import RawExport from "@/components/AdminArea/RawExport.vue";
 		CourseExport,
 	},
 })
-export default class DataExport extends Vue {}
+export default class DataExport extends Vue {
+	courses: Array<courseContainer> = [];
+	async mounted() {
+		await getAllCourses().then(response => (this.courses = response));
+	}
+}
 </script>
 
 <style scoped lang="scss"></style>
