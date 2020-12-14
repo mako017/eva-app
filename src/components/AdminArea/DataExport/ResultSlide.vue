7bed5e1af403795cfd641afe7ba530af65fc3beb
marco Koch <marco.koch@uni-saarland.de>
2020-12-14 13:49:26 +0100

    progress on bar chart

--- src/components/AdminArea/DataExport/ResultSlide.vue
+++ src/components/AdminArea/DataExport/ResultSlide.vue
@@ -1,51 +1,49 @@
 <template>
-	<div>
-		{{ evaResult }}
-		<canvas id="bars"></canvas>
+	<div class="root">
+		<div class="data">
+			<span>Titel: </span>
+			<span>Doz: </span>
+			<span>N = {{ evaResult[1].N }}</span>
+			<span>M = {{ evaResult[1].mean }}</span>
+			<span>SD = {{ evaResult[1].sd }}</span>
+		</div>
+		<Bars :data="[evaResult[1].A, evaResult[1].B, evaResult[1].C, evaResult[1].D, evaResult[1].E]" :labels="['++', '+', 'o', '-', '--']" :colors="['black', 'black', 'black', 'black', 'black']" />
 	</div>
 </template>
 
 <script lang="ts">
-import Chart from "chart.js";
+import Bars from "@/components/AdminArea/DataExport/Bars.vue";
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { EvaResult, RawResults, SingleResult } from "@/assets/ts/results.ts";
 
-@Component
+@Component({
+	components: {
+		Bars,
+	},
+})
 export default class ResultSlide extends Vue {
 	@Prop() results!: Array<RawResults>;
 	private evaResult: Array<SingleResult> = [];
-
-	private labels = ["++", "+", "o", "-", "--"];
-	@Prop({ default: [] }) readonly colors!: Array<string>;
-	@Prop({ default: [] }) readonly data!: Array<number>;
-	@Prop({
-		default: () => {
-			return Chart.defaults.doughnut;
-		},
-	})
-	readonly options: object | undefined;
-
 	mounted() {
 		this.evaResult = new EvaResult(this.results).compute();
-		this.createChart({
-			datasets: [
-				{
-					data: this.data,
-				},
-			],
-			labels: this.labels,
-		});
-	}
-	createChart(chartData: object) {
-		const canvas = document.getElementById("doughnut") as HTMLCanvasElement;
-		const options = {
-			type: "doughnut",
-			data: chartData,
-			options: this.options,
-		};
-		new Chart(canvas, options);
 	}
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.root {
+	width: 100%;
+	display: grid;
+	grid-template-columns: 2fr 3fr;
+	* {
+		width: 100%;
+	}
+}
+.data {
+	margin: 1rem 0.5rem;
+	display: flex;
+	flex-direction: column;
+	text-align: start;
+	line-height: 1.2rem;
+}
+</style>
