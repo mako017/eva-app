a39105e93fec1cfbe7fbb3c348aeacf6c6dca7b3
marco Koch <marco.koch@uni-saarland.de>
2020-12-14 12:55:04 +0100

    scaffolding for diagrams

--- src/components/AdminArea/DataExport/ResultSlide.vue
+++ src/components/AdminArea/DataExport/ResultSlide.vue
@@ -1,10 +1,12 @@
 <template>
 	<div>
 		{{ evaResult }}
+		<canvas id="bars"></canvas>
 	</div>
 </template>
 
 <script lang="ts">
+import Chart from "chart.js";
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { EvaResult, RawResults, SingleResult } from "@/assets/ts/results.ts";
 
@@ -12,8 +14,36 @@ import { EvaResult, RawResults, SingleResult } from "@/assets/ts/results.ts";
 export default class ResultSlide extends Vue {
 	@Prop() results!: Array<RawResults>;
 	private evaResult: Array<SingleResult> = [];
+
+	private labels = ["++", "+", "o", "-", "--"];
+	@Prop({ default: [] }) readonly colors!: Array<string>;
+	@Prop({ default: [] }) readonly data!: Array<number>;
+	@Prop({
+		default: () => {
+			return Chart.defaults.doughnut;
+		},
+	})
+	readonly options: object | undefined;
+
 	mounted() {
 		this.evaResult = new EvaResult(this.results).compute();
+		this.createChart({
+			datasets: [
+				{
+					data: this.data,
+				},
+			],
+			labels: this.labels,
+		});
+	}
+	createChart(chartData: object) {
+		const canvas = document.getElementById("doughnut") as HTMLCanvasElement;
+		const options = {
+			type: "doughnut",
+			data: chartData,
+			options: this.options,
+		};
+		new Chart(canvas, options);
 	}
 }
 </script>
